91a3637989cca3d3fcaf3c7daa3d653fe9d01779
Robert Ellegate <rje6459@rit.edu>
2019-11-24 21:44:16 -0500

    Made significant progress on script

--- create-nikto-targets.py
+++ create-nikto-targets.py
@@ -1,22 +1,49 @@
+import sys
+import os
 import subprocess
-from defusedxml import ElementTree
+import argparse
+import defusedxml
 
-# def _writeToTXT(file):
-#     with open(filename, 'w') as f:
+
+def _writeToTXT(file):
+    if os.path.exists(file):
+        os.remove(file)
+    f = open(file, "a")
+    return f
+
+
+def _parse_args(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "targets", help="targets can be hostnames, IP addresses, networks, etc."
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        default="nikto-targets.txt",
+        help="output filename (default: %(default)s)",
+    )
+
+    return parser.parse_args(args)
+
 
 def main():
-    ip_addr = "127.0.0.1"
+    args = _parse_args(sys.argv[1:])
+
+    txt_writer = _writeToTXT(args.output)
+
     process = subprocess.run(
-        ["nmap", "--open", "-oX", "-", ip_addr],
+        ["nmap", "--open", "-oX", "-", args.targets],
         stdout=subprocess.PIPE,
         universal_newlines=True,
     )
-    
-    tree = ElementTree.fromstring(process.stdout)
-    for host in tree.findall('host'):
-        addr = host.get('address')
-        print(host)
-        # print(ElementTree.tostring(host, encoding='utf8', method='xml').decode())
+
+    tree = defusedxml.ElementTree.fromstring(process.stdout)
+    for host in tree.findall("host"):
+        ip_addr = host.find("address").get("addr")
+        for port in host.find("ports").findall("port"):
+            port_id = port.get("portid")
+            txt_writer.write(ip_addr + ":" + port_id + "\n")
 
 
 if __name__ == "__main__":
